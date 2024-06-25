5fc4e5b81d64768a663d2eccfb60fb32926a8f0e
yujin <jinietree20@gmail.com>
2024-06-25 17:51:19 +0900

    [yujin] add project

---
+++ yujin/ArtMuseum/ArtMuseum/ArtMuseumApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtMuseumApp.swift
+//  ArtMuseum
+//
+//  Created by 정유진 on 6/22/24.
+//
+
+import SwiftUI
+
+@main
+struct ArtMuseumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
